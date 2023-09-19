8c09d222b502c848246eb5feda231b3c6118da34
carlscar1 <79236208+carlscar1@users.noreply.github.com>
2023-09-19 09:30:29 -0400

    Initial Commit

---
+++ CIS357HW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CIS357HW2
+//
+//  Created by Caroline Carlson on 9/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CIS357HW2UITests/CIS357HW2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CIS357HW2UITestsLaunchTests.swift
+//  CIS357HW2UITests
+//
+//  Created by Caroline Carlson on 9/19/23.
+//
+
+import XCTest
+
+final class CIS357HW2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
